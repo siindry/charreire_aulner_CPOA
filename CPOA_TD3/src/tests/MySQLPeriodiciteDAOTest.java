8d2818187935f3cd4dae01179f172f069e8dece3
Emma Charreire <charreir2u@iutm-inf-f33-05.iutmetz.site.univ-lorraine.fr>
2019-10-05 11:21:48 +0200

    modification PeriodiciteSQL Test(create,delete,get)

--- CPOA_TD3/src/tests/MySQLPeriodiciteDAOTest.java
+++ CPOA_TD3/src/tests/MySQLPeriodiciteDAOTest.java
@@ -1,33 +1,87 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import java.sql.Connection;
+import org.junit.Before;
+import org.junit.Test;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import dao.connexion.Connexion;
 import dao.factory.DAOFactory;
-import dao.mysql.MySQLPeriodiciteDAO;
 import enumeration.EPersistance;
 import metier.Periodicite;
 
 class MySQLPeriodiciteDAOTest {
 	
-
+	private DAOFactory doas;
 	
+	@Before
+	public void setUp() {
+		doas = DAOFactory.getDAOFactory(EPersistance.MYSQL);
+	}
 	
 	// une classe = une classeTest et  une méthode = une méthodeTest
 
 	
 	@Test
-	void testCreateWork() {
-		DAOFactory doas = DAOFactory.getDAOFactory(EPersistance.MYSQL);
+	public void testCreateWork() {
+		System.out.println("\nCreation fonctionne :");
 		Periodicite ptest1 = new Periodicite(0,"Trimestriel");
-		
+
 		assertTrue(doas.getPeriodiciteDAO().create(ptest1));
 	}
+	
+	@Test
+	public void testDeleteWork() {
+		
+		System.out.println("\nSuppression fonctionne :");
+		
+		Periodicite ptest1 = new Periodicite(70,"Trimestriel");
+		doas.getPeriodiciteDAO().create(ptest1);
+		
+		assertTrue(doas.getPeriodiciteDAO().delete(ptest1));
+
+	}
+	
+	@Test
+	public void testGetWork() {
+		
+		System.out.println("\nRecherche fonctionne :");
+		
+		Periodicite ptest1 = new Periodicite(70,"Trimestriel");
+		doas.getPeriodiciteDAO().create(ptest1);
+		int id = ptest1.getId_periode();
+		
+		assertNotNull(doas.getPeriodiciteDAO().getById(id));
+
+	}
+	
+	/*@Test
+	void testDeleteNotWork() {
+		
+		System.out.println("\nSuppression fonctionne :");
+		
+		DAOFactory doas = DAOFactory.getDAOFactory(EPersistance.MYSQL);
+		Periodicite ptest1 = new Periodicite(70,"Trimestriel");
+		Periodicite ptest1 = new Periodicite(70,"Trimestriel");
+		doas.getPeriodiciteDAO().create(ptest1);
+		
+		assertTrue(doas.getPeriodiciteDAO().create(ptest1));
+
+	}*/
+	
+	/*@Test
+	void testUpdateWork() {
+		
+		DAOFactory doas = DAOFactory.getDAOFactory(EPersistance.MYSQL);
+		Periodicite ptest1 = new Periodicite(0,"Trimestriel");
+		Periodicite ptest2 = new Periodicite(0,"Trimestriel");
+		assertTrue(doas.getPeriodiciteDAO().create(ptest1));
+
+	}*/
+	
+	
+	
+	
 	
 
 
