8e86325fa2810593cfc0ec51178b75fe5a050cb4
user <user@3662PC>
2019-10-17 13:06:17 +0200

    2nd commit

--- CPOA_TD4_FX/src/application/Main.java
+++ CPOA_TD4_FX/src/application/Main.java
@@ -38,7 +38,7 @@ public class Main extends Application {
 			Scene scene = new Scene((VBox) root, 600, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Ajout Revue");
+			primaryStage.setTitle("Revue");
 			primaryStage.show();
 			
 			
